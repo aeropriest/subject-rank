b1c30a8fbe2388808de90ef5e0ac53d879f02490
aeropriest <ashok.jaiswal@gmail.com>
2023-10-16 23:10:27 +0800

    * Finished it

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from "@/components/ui/button"
-import { Check, ChevronsUpDown, PlusSquare, Trash2 } from "lucide-react"
+import { Check, ChevronsUpDown, PlusSquare, Trash2, ArrowUpDown } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -44,10 +44,11 @@ const Combobox = ({ data, value, setValue, handleChange }) => {
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
+          className="w-[200px] justify-between p-2 text-xs font-normal text-left"
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+
         >
           {value
             ? data.find((option) => option.value === value)?.label
@@ -61,6 +62,7 @@ const Combobox = ({ data, value, setValue, handleChange }) => {
           <CommandGroup>
             {data.map((option) => (
               <CommandItem
+                className="text-xs font-normal text-left"
                 key={option.value}
                 onSelect={(currentValue) => {
                   setValue(currentValue === value ? "" : currentValue);
@@ -106,27 +108,77 @@ const YourComponent = () => {
     setData(newData);
   };
 
+
+
+  const handleDeleteRow = (index) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  const [newSubject, setNewSubject] = useState(''); // State to hold the new subject
+
+  const handleAddRow = () => {
+    const newRow = { subject: newSubject, difficulty: '', enjoyment: '', workload: '' };
+    const newData = [...data, newRow];
+    setData(newData);
+    setNewSubject(''); // Clear the input field
+  };
+
+  const [sortAscending, setSortAscending] = useState(true); // Track sorting direction
+
+  const handleSort = () => {
+    const newData = [...data];
+    newData.sort((a, b) => {
+      const sumA = parseInt(a.difficulty) + parseInt(a.enjoyment) + parseInt(a.workload);
+      const sumB = parseInt(b.difficulty) + parseInt(b.enjoyment) + parseInt(b.workload);
+      if (sortAscending) {
+        return sumA - sumB;
+      } else {
+        return sumB - sumA;
+      }
+    });
+
+    setData(newData);
+    setSortAscending(!sortAscending); // Toggle sorting direction
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-lg p-4">
       <table className="w-full border-collapse">
         <thead>
           <tr>
-            <th className="p-2 text-left">Subject</th>
-            <th className="p-2 text-left">Difficulty</th>
-            <th className="p-2 text-left">Enjoyment</th>
-            <th className="p-2 text-left">Workload</th>
-            <th className="p-2 text-left">Rank</th>
+            <th className="p-2 text-sm font-normal text-left"></th>
+            <th className="p-2 text-sm font-normal text-left">Subject</th>
+            <th className="p-2 text-sm font-normal text-left">Difficulty</th>
+            <th className="p-2 text-sm font-normal text-left">Enjoyment</th>
+            <th className="p-2 text-sm font-normal text-left">Workload</th>
+            <th className="p-2 text-sm font-normal text-left">Rank
+              <Button
+                onClick={handleSort}
+                className="p-0 bg-transparent border-none"
+                style={{ backgroundColor: 'transparent' }}
+              >
+                <ArrowUpDown color="black" size={15} style={{ backgroundColor: 'transparent' }} />
+              </Button>
+            </th>
           </tr>
         </thead>
         <tbody>
           {data.map((row, index) => (
             <tr key={index}>
               <td className="p-2">
+                <Button className="p-0 bg-transparent border-none"
+                  style={{ backgroundColor: 'transparent' }}
+                  onClick={() => handleDeleteRow(index)}><Trash2 color="red" size={15} style={{ backgroundColor: 'transparent' }} /></Button>
+              </td>
+              <td className="p-2">
                 <Input
+                  className="p-2 text-xs font-normal text-left"
                   type="text"
                   value={row.subject}
                   onChange={(event) => handleChange(event, index, 'subject')}
-                  className="w-full p-1 border rounded-md"
+                // className="w-full p-1 border rounded-md"
                 />
               </td>
               <td className="p-2">
@@ -159,11 +211,27 @@ const YourComponent = () => {
                   handleChange={(event) => handleChange(event, index, 'workload')}
                 />
               </td>
-              <td className="p-2">
+              <td className="p-2 text-xs font-normal text-left">
                 {parseInt(row.difficulty) + parseInt(row.enjoyment) + parseInt(row.workload)}
               </td>
             </tr>
           ))}
+          <tr>
+            <td className="p-2">
+              <Button className="p-0 bg-transparent border-none"
+                style={{ backgroundColor: 'transparent' }}
+                onClick={handleAddRow}><PlusSquare color="blue" size={15} style={{ backgroundColor: 'transparent' }} /></Button>
+            </td>
+            <td className="p-2">
+              <Input
+                className="w-[200px] justify-between p-2 text-xs font-normal text-left"
+                type="text"
+                placeholder="Subject..."
+                // className="w-full p-1 border rounded-md"
+                onChange={(val) => setNewSubject(val.target.value)}
+              />
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
