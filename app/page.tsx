37ca9dc3f93b9a593267a6c62829c37179e2c4a9
aeropriest <ashok.jaiswal@gmail.com>
2023-10-16 12:51:26 +0800

    Table is ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,284 @@
-import Image from 'next/image'
+"use client"
+import React, { useEffect, useReducer } from "react";
+import "./style.css";
+import makeData from "./makeData";
+import Table from "./Table";
+import { randomColor, shortId } from "./utils";
+import { grey } from "./colors";
 
-export default function Home() {
+function reducer(state, action) {
+  switch (action.type) {
+    case "add_option_to_column":
+      const optionIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, optionIndex),
+          {
+            ...state.columns[optionIndex],
+            options: [
+              ...state.columns[optionIndex].options,
+              { label: action.option, backgroundColor: action.backgroundColor }
+            ]
+          },
+          ...state.columns.slice(optionIndex + 1, state.columns.length)
+        ]
+      };
+    case "add_row":
+      return {
+        ...state,
+        skipReset: true,
+        data: [...state.data, {}]
+      };
+    case "update_column_type":
+      const typeIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      switch (action.dataType) {
+        case "number":
+          if (state.columns[typeIndex].dataType === "number") {
+            return state;
+          } else {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: isNaN(row[action.columnId])
+                  ? ""
+                  : Number.parseInt(row[action.columnId])
+              }))
+            };
+          }
+        case "select":
+          if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              skipReset: true
+            };
+          } else {
+            let options = [];
+            state.data.forEach((row) => {
+              if (row[action.columnId]) {
+                options.push({
+                  label: row[action.columnId],
+                  backgroundColor: randomColor()
+                });
+              }
+            });
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                {
+                  ...state.columns[typeIndex],
+                  dataType: action.dataType,
+                  options: [...state.columns[typeIndex].options, ...options]
+                },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              skipReset: true
+            };
+          }
+        case "text":
+          if (state.columns[typeIndex].dataType === "text") {
+            return state;
+          } else if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ]
+            };
+          } else {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: row[action.columnId] + ""
+              }))
+            };
+          }
+        default:
+          return state;
+      }
+    case "update_column_header":
+      const index = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, index),
+          { ...state.columns[index], label: action.label },
+          ...state.columns.slice(index + 1, state.columns.length)
+        ]
+      };
+    case "update_cell":
+      return {
+        ...state,
+        skipReset: true,
+        data: state.data.map((row, index) => {
+          if (index === action.rowIndex) {
+            return {
+              ...state.data[action.rowIndex],
+              [action.columnId]: action.value
+            };
+          }
+          return row;
+        })
+      };
+    case "add_column_to_left":
+      const leftIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      let leftId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, leftIndex),
+          {
+            id: leftId,
+            label: "Column",
+            accessor: leftId,
+            dataType: "text",
+            created: action.focus && true,
+            options: []
+          },
+          ...state.columns.slice(leftIndex, state.columns.length)
+        ]
+      };
+    case "add_column_to_right":
+      const rightIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      const rightId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, rightIndex + 1),
+          {
+            id: rightId,
+            label: "Column",
+            accessor: rightId,
+            dataType: "text",
+            created: action.focus && true,
+            options: []
+          },
+          ...state.columns.slice(rightIndex + 1, state.columns.length)
+        ]
+      };
+    case "delete_column":
+      const deleteIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, deleteIndex),
+          ...state.columns.slice(deleteIndex + 1, state.columns.length)
+        ]
+      };
+    case "enable_reset":
+      return {
+        ...state,
+        skipReset: false
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, makeData(10));
+
+  useEffect(() => {
+    dispatch({ type: "enable_reset" });
+  }, [state.data, state.columns]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflowX: "hidden"
+      }}
+    >
+      <div
+        style={{
+          height: 120,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <h1 style={{ color: grey(800) }}>Editable React Table</h1>
+      </div>
+      <div style={{ overflow: "auto", display: "flex" }}>
+        <div
+          style={{
+            flex: "1 1 auto",
+            padding: "1rem",
+            maxWidth: 1000,
+            marginLeft: "auto",
+            marginRight: "auto"
+          }}
+        >
+          <Table
+            columns={state.columns}
+            data={state.data}
+            dispatch={dispatch}
+            skipReset={state.skipReset}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        style={{
+          height: 140,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <p style={{ color: grey(600) }}>
+          Built by{" "}
+          <a
+            href="https://twitter.com/thesysarch"
+            style={{ color: grey(600), fontWeight: 600 }}
+          >
+            @thesysarch
+          </a>
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default App;
